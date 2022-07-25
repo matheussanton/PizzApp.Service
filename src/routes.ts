fb8a25687ca5dd3c79499226e500fc2eceafaecb
Matheus Santon Galter <matheussantong@gmail.com>
2022-07-24 23:17:01 -0300

    First route and service concepts

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+
+import { CreateUserController } from "./controllers/user/CreateUserController"
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-    return res.json({
-        ok: true,
-        version: "1.0",
-        name: "PizzApp",
-    });
-});
+//User Routes
+router.post("/users", new CreateUserController().handle);
 
 export { router };
